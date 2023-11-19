e1b5d87629c75e41958516a9bb6163e470ce881b
Mohamed Khaled Gomaa <mkgomaa@banquemisr.com>
2023-11-19 11:32:41 +0200

    Initial Commit

---
+++ Crypto SwiftUI/Crypto_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_SwiftUIApp.swift
+//  Crypto SwiftUI
+//
+//  Created by Mohamed Khaled Gomaa on 19/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
